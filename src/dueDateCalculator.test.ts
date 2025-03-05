8a6baacecedd2176734b43b650a8e07f8c8dbbdf
hewayman <hewayman@gmail.com>
2025-03-04 21:37:31 -0500

    Move tests

--- src/dueDateCalculator.test.ts
+++ src/dueDateCalculator.test.ts
@@ -1,4 +1,44 @@
-import { calculateDueDate } from './dueDateCalculator';
+import {
+  adjustDateToBusinessHours,
+  calculateDueDate,
+} from './dueDateCalculator';
+
+describe('adjustDateToBusinessHours', () => {
+  it('adjusts a submit date before 9AM to 9AM of the same day', () => {
+    const submitDate = new Date('2025-03-04T06:51:27');
+    expect(adjustDateToBusinessHours(submitDate)).toEqual(
+      new Date('2025-03-04T09:00:00')
+    );
+  });
+
+  it('adjusts a submit date at 5PM to 9AM on the next business day', () => {
+    const submitDate = new Date('2025-03-04T17:00:00');
+    expect(adjustDateToBusinessHours(submitDate)).toEqual(
+      new Date('2025-03-05T09:00:00')
+    );
+  });
+
+  it('adjusts a submit date on Friday after 5PM to the next Monday at 9AM', () => {
+    const submitDate = new Date('2025-03-07T22:36:47');
+    expect(adjustDateToBusinessHours(submitDate)).toEqual(
+      new Date('2025-03-10T09:00:00')
+    );
+  });
+
+  it('adjusts a submit date on Saturday to the next Monday at 9AM', () => {
+    const submitDate = new Date('2025-03-01T12:36:47');
+    expect(adjustDateToBusinessHours(submitDate)).toEqual(
+      new Date('2025-03-03T09:00:00')
+    );
+  });
+
+  it('adjusts a submit date on Sunday to the next Monday at 9AM', () => {
+    const submitDate = new Date('2025-03-02T12:36:47');
+    expect(adjustDateToBusinessHours(submitDate)).toEqual(
+      new Date('2025-03-03T09:00:00')
+    );
+  });
+});
 
 describe('dueDateCalculator', () => {
   it('returns a Date object', () => {
@@ -23,135 +63,91 @@ describe('dueDateCalculator', () => {
     );
   });
 
-  describe('when the submit time is during working hours', () => {
-    it('handles a turnaround time of 0', () => {
-      const submitDate = new Date('2025-03-04T12:00:00');
-      const turnaroundTime = 0;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-04T12:00:00')
-      );
-    });
-
-    it('handles a turnaround time under 1 hour', () => {
-      const submitDate = new Date('2025-03-04T12:00:00');
-      const turnaroundTime = 0.5;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-04T12:30:00')
-      );
-    });
-
-    it('calculates a due date within the same working day', () => {
-      const submitDate = new Date('2025-03-04T09:32:04');
-      const turnaroundTime = 6;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-04T15:32:04')
-      );
-    });
-
-    it('calculates a due date for a turnaround time of exactly one workday', () => {
-      const submitDate = new Date('2025-03-04T09:00:00');
-      const turnaroundTime = 8;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-04T17:00:00')
-      );
-    });
-
-    it('calculates a due date for multi-day turnaround', () => {
-      const submitDate = new Date('2025-03-04T14:25:02');
-      const turnaroundTime = 48;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-12T14:25:02')
-      );
-    });
-
-    it('calculates a due date across a weekend', () => {
-      const submitDate = new Date('2025-03-07T09:25:02');
-      const turnaroundTime = 18;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-11T11:25:02')
-      );
-    });
-
-    it('calculates a due date for a midnight submit date', () => {
-      const submitDate = new Date('2025-03-04T00:00:00');
-      const turnaroundTime = 8;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-04T17:00:00')
-      );
-    });
-
-    it('calculates a due date into the next year', () => {
-      const submitDate = new Date('2025-12-30T09:25:02');
-      const turnaroundTime = 18;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2026-01-01T11:25:02')
-      );
-    });
-
-    it('handles a leap year', () => {
-      const submitDate = new Date('2024-02-28T09:25:02');
-      const turnaroundTime = 18;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2024-03-01T11:25:02')
-      );
-    });
-
-    it('handles submission on February 29th in a leap year', () => {
-      const submitDate = new Date('2024-02-29T09:25:02');
-      const turnaroundTime = 8;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2024-03-01T09:25:02')
-      );
-    });
-
-    it('handles a non-leap year', () => {
-      const submitDate = new Date('2025-02-28T09:25:02');
-      const turnaroundTime = 18;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-04T11:25:02')
-      );
-    });
+  it('handles a turnaround time of 0', () => {
+    const submitDate = new Date('2025-03-04T12:00:00');
+    const turnaroundTime = 0;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-04T12:00:00')
+    );
   });
 
-  describe('when the submit time is during non-working hours', () => {
-    it('adjusts a submit time before 9AM to 9AM of the same day', () => {
-      const submitDate = new Date('2025-03-04T06:51:27');
-      const turnaroundTime = 2;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-04T11:00:00')
-      );
-    });
+  it('handles a turnaround time under 1 hour', () => {
+    const submitDate = new Date('2025-03-04T12:00:00');
+    const turnaroundTime = 0.5;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-04T12:30:00')
+    );
+  });
 
-    it('adjusts a submit time at 5PM to 9AM on the next business day', () => {
-      const submitDate = new Date('2025-03-04T17:00:00');
-      const turnaroundTime = 1;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-05T10:00:00')
-      );
-    });
+  it('calculates a due date within the same working day', () => {
+    const submitDate = new Date('2025-03-04T09:32:04');
+    const turnaroundTime = 6;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-04T15:32:04')
+    );
+  });
 
-    it('adjusts a submit date on Friday after 5PM to Monday at 9AM', () => {
-      const submitDate = new Date('2025-03-07T22:36:47');
-      const turnaroundTime = 2;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-10T11:00:00')
-      );
-    });
+  it('calculates a due date for a turnaround time of exactly one workday', () => {
+    const submitDate = new Date('2025-03-04T09:00:00');
+    const turnaroundTime = 8;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-04T17:00:00')
+    );
+  });
 
-    it('adjusts a submit date on Saturday to Monday at 9AM', () => {
-      const submitDate = new Date('2025-03-01T12:36:47');
-      const turnaroundTime = 2;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-03T11:00:00')
-      );
-    });
+  it('calculates a due date for multi-day turnaround', () => {
+    const submitDate = new Date('2025-03-04T14:25:02');
+    const turnaroundTime = 48;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-12T14:25:02')
+    );
+  });
 
-    it('adjusts a submit date on Sunday to Monday at 9AM', () => {
-      const submitDate = new Date('2025-03-02T12:36:47');
-      const turnaroundTime = 2;
-      expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
-        new Date('2025-03-03T11:00:00')
-      );
-    });
+  it('calculates a due date across a weekend', () => {
+    const submitDate = new Date('2025-03-07T09:25:02');
+    const turnaroundTime = 18;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-11T11:25:02')
+    );
+  });
+
+  it('calculates a due date for a midnight submit date', () => {
+    const submitDate = new Date('2025-03-04T00:00:00');
+    const turnaroundTime = 8;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-04T17:00:00')
+    );
+  });
+
+  it('calculates a due date into the next year', () => {
+    const submitDate = new Date('2025-12-30T09:25:02');
+    const turnaroundTime = 18;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2026-01-01T11:25:02')
+    );
+  });
+
+  it('handles a leap year', () => {
+    const submitDate = new Date('2024-02-28T09:25:02');
+    const turnaroundTime = 18;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2024-03-01T11:25:02')
+    );
+  });
+
+  it('handles submission on February 29th in a leap year', () => {
+    const submitDate = new Date('2024-02-29T09:25:02');
+    const turnaroundTime = 8;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2024-03-01T09:25:02')
+    );
+  });
+
+  it('handles a non-leap year', () => {
+    const submitDate = new Date('2025-02-28T09:25:02');
+    const turnaroundTime = 18;
+    expect(calculateDueDate(submitDate, turnaroundTime)).toEqual(
+      new Date('2025-03-04T11:25:02')
+    );
   });
 });
